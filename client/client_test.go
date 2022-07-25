3cd36b89f0b44cd5e9268d9ef718d947713b2a02
Luca Georges Francois <luca.georges-francois@epitech.eu>
2022-07-25 17:14:21 +0200

    feat(mock_server): support topics filters while forging responses
    Signed-off-by: Luca Georges Francois <luca.georges-francois@epitech.eu>

--- client/client_test.go
+++ client/client_test.go
@@ -14,7 +14,7 @@ import (
 func TestSubscribeToValidBids(t *testing.T) {
 	var wg sync.WaitGroup
 
-	srv := testing2.NewMockServer(DataEndpoint, topics.BuilderBidValid)
+	srv := testing2.NewMockServer(DataEndpoint)
 	defer func() {
 		err := srv.Close()
 		assert.NoError(t, err)
@@ -48,8 +48,8 @@ func TestSubscribeToValidBids(t *testing.T) {
 	data := <-res
 	assert.Equal(t, data.Message.EventType, topics.BuilderBidValid)
 
-	// data = <-res
-	// assert.Equal(t, data.Message.EventType, topics.ProposerGetHeader)
+	data = <-res
+	assert.Equal(t, data.Message.EventType, topics.ProposerGetHeader)
 
 	client.Unsubscribe()
 }
