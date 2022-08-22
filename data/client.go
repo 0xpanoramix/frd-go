820c5d2256bfb47f9bc255b9114a9f3164704001
Luca Georges Francois <luca.georges-francois@epitech.eu>
2022-08-22 12:05:07 -0700

    feat: add first method to get bid traces from relay
    Signed-off-by: Luca Georges Francois <luca.georges-francois@epitech.eu>

--- data/client.go
+++ data/client.go
@@ -1,7 +1,11 @@
 package data
 
 import (
+	"encoding/json"
+	"fmt"
+	"github.com/flashbots/go-boost-utils/types"
 	"net/http"
+	"strings"
 	"time"
 )
 
@@ -19,4 +23,73 @@ func NewTransparencyClient(baseURL string, timeout time.Duration) *TransparencyC
 		},
 		baseURL: baseURL,
 	}
+}
+
+type GetProposerPayloadsDeliveredOptions struct {
+	Slot        uint64
+	Cursor      uint64
+	Limit       uint64
+	BlockHash   string
+	BlockNumber uint64
+}
+
+func (o *GetProposerPayloadsDeliveredOptions) ToQueryParameters() string {
+	var args []string
+
+	if o.Slot > 0 {
+		args = append(args, fmt.Sprintf("slot=%d", o.Slot))
+	}
+
+	if o.Cursor > 0 {
+		args = append(args, fmt.Sprintf("cursor=%d", o.Cursor))
+	}
+
+	if o.Limit > 0 {
+		args = append(args, fmt.Sprintf("limit=%d", o.Limit))
+	}
+
+	if o.BlockHash != "" {
+		args = append(args, fmt.Sprintf("block_hash=%s", o.BlockHash))
+	}
+
+	if o.BlockNumber > 0 {
+		args = append(args, fmt.Sprintf("block_number=%d", o.BlockNumber))
+	}
+
+	params := strings.Join(args, "&")
+	if len(params) > 0 {
+		params = "?" + params
+	}
+
+	return params
+}
+
+// GetProposerPayloadsDelivered provides bids for payloads that were delivered to proposers.
+//nolint:lll
+func (c *TransparencyClient) GetProposerPayloadsDelivered(options *GetProposerPayloadsDeliveredOptions) ([]types.BidTrace, error) {
+	// Creates the URL using the provided the base url and options.
+	path := "/relay/v1/data/bidtraces/proposer_payload_delivered"
+	url := c.baseURL + path
+
+	if options != nil {
+		url += options.ToQueryParameters()
+	}
+
+	res, err := http.Get(url) //nolint
+	if err != nil {
+		return nil, err
+	}
+
+	// Handle error in the response.
+	if res.StatusCode >= 400 {
+		return nil, fmt.Errorf("failed request to %s with status code %d", url, res.StatusCode)
+	}
+
+	// Extract bid traces from response.
+	var traces []types.BidTrace
+	if err = json.NewDecoder(res.Body).Decode(&traces); err != nil {
+		return nil, err
+	}
+
+	return traces, nil
 }
