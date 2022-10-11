08f1a3ed2913b50042823cbe9210fb684ed64563
Luca Georges Francois <luca.georges-francois@kiln.fi>
2022-10-10 17:30:38 -0700

    fix: remove deprecated Flashbots Relays
    Signed-off-by: Luca Georges Francois <luca.georges-francois@kiln.fi>

--- constants/relay.go
+++ constants/relay.go
@@ -1,7 +1,5 @@
 package constants
 
 const FlashbotsRelayMainnet = "https://boost-relay.flashbots.net"
-const FlashbotsRelayKiln = "https://builder-relay-kiln.flashbots.net"
-const FlashbotsRelayRopsten = "https://builder-relay-ropsten.flashbots.net"
 const FlashbotsRelaySepolia = "https://builder-relay-sepolia.flashbots.net"
 const FlashbotsRelayGoerli = "https://builder-relay-goerli.flashbots.net"
